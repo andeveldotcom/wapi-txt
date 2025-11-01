c6de9cec9170e9940a4df5c085c31060dade1dd4
andeveldotcom <150321541+andeveldotcom@users.noreply.github.com>
2025-11-01 21:00:23 +0100

    Change of version information (Linux)

--- My Project/AssemblyInfo.net8.vb
+++ My Project/AssemblyInfo.net8.vb
@@ -5,8 +5,8 @@ Imports System
 Imports System.Reflection
 
 <Assembly: AssemblyCompany("andevel")>
-<Assembly: AssemblyFileVersion("1.0.0.1")>
-<Assembly: AssemblyInformationalVersion("1.0.1-linux")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: AssemblyInformationalVersion("1.0.0-linux")>
 <Assembly: AssemblyProduct("wapi-txt")>
 <Assembly: AssemblyTitle("wapi-txt")>
-<Assembly: AssemblyVersion("1.0.0.1")>
+<Assembly: AssemblyVersion("1.0.0.0")>
